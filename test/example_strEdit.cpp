05f1812ded84539f0f446fb1372e5c0d2d164cdb
admiswalker <admiswalker@gmail.com>
2023-03-29 00:38:49 +0900

    WIP

--- test/example_strEdit.cpp
+++ test/example_strEdit.cpp
@@ -2,10 +2,10 @@
 #include "../src/example_strEdit.cpp"
 
 
-TEST(example, joint_a_b){
+TEST(example_strEdit, joint_a_b){
     ASSERT_STREQ(joint_a_b("a", "b").c_str(), "ab");
 }
-TEST(example, joint_a_b_with_x){
+TEST(example_strEdit, joint_a_b_with_x){
     ASSERT_STREQ(joint_a_b_with_x("a", "b", ", ").c_str(), "a, b");
 //    ASSERT_TRUE(false); // Force to raise error
 }
